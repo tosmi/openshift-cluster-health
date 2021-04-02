11ff4905223bd7891837f334b1d93c1c0c64891a
Toni Schmidbauer <toni@stderr.at>
2021-04-02 15:45:24 +0200

    start using cobra for command line flags

--- cmd/och/och.go
+++ cmd/och/och.go
@@ -2,9 +2,26 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/spf13/cobra"
 	"github.com/tosmi/openshift-cluster-health/pkg/version"
 )
 
+var (
+	rootCmd = &cobra.Command{
+		Use:   "och",
+		Short: "OpenShift Cluster Healthcheck",
+		Long:  `och runs various check againts a running OpenShift Cluster. `,
+	}
+)
+
+func init(
+	log.SetOutput(os.Stdout)
+)
+
 func main() {
+
 	fmt.Println("Hello World! This is version", version.VERSION, version.GITCOMMIT)
+
+	rootCmd.Execute()
 }
